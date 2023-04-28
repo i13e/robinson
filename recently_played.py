1501e80e8d69e2ecc5a6233c122f48bd8b3205fb
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-04-28 10:38:01 -0400

    feat: Add `recently_played.py` to extract and store played tracks
    This commit adds a new file, `recently_played.py`, that performs an ETL
    (extract, transform, load) process to extract the played tracks by a user from
    the start of the day and store them in an sqlite database. Here are the
    details:
    
    - Extract: `recently_played.py` uses the Spotify API to extract up to 50 tracks
      played by a user from the start of the day.
    - Transform: The extracted data is then transformed into a format that is
      suitable for storage in an sqlite database.
    - Validate: The transformed data is validated to ensure that all required fields
      are present and have valid values.
    - Load: Finally, the validated data is loaded into an sqlite database for
      future use.
    
    By adding this file, we can easily keep track of the tracks that a user has
    listened to recently and use this information to discover patterns,
    build playlists, and make personalized recommendations.

---
+++ recently_played.py
@@ -0,0 +1,191 @@
+import sqlite3
+
+import datetime as dt
+from dotenv import load_dotenv
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import sqlalchemy
+import sqlalchemy.exc
+
+load_dotenv()
+
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+
+
+class EmptyDataFrameError(Exception):
+    """Raised when the input DataFrame is empty"""
+
+
+class PrimaryKeyError(Exception):
+    """Raised when the primary key check fails"""
+
+
+class NullValuesError(Exception):
+    """Raised when null values are found in the input DataFrame"""
+
+
+class InvalidTimestampError(Exception):
+    """Raised when at least one song has an invalid timestamp"""
+
+
+def extract_recently_played() -> dict:
+    """
+    Extracts recently played songs from Spotify API.
+
+    Returns:
+        dict: Dictionary containing information about recently played songs.
+    """
+
+    # Set user permissions
+    scope = ["user-library-read", "user-read-recently-played"]
+
+    # Authenticate user
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    # Convert time to Unix timestamp in miliseconds
+    now = dt.datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    timestamp = int(midnight.timestamp()) * 10**3
+
+    # Download all songs the user has listened to from the start of today
+    if not (data := sp.current_user_recently_played(after=timestamp)):
+        raise ValueError("No data found for recent song plays.")
+
+    return data
+
+
+def transform_data(data: dict) -> pd.DataFrame:
+    """
+    Transforms data extracted from Spotify API into a pandas DataFrame.
+
+    Args:
+        data (dict): Dictionary containing information about recently
+        played songs.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing song names, artist names,
+        played_at and timestamps.
+    """
+
+    # Extract song names, artist names, played_at and timestamps from the response
+    song_names = []
+    artist_names = []
+    played_at = []
+    timestamps = []
+
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at.append(song["played_at"])
+        timestamps.append(song["played_at"][:10])
+
+    # Prepare a dictionary to turn it into a pandas dataframe
+    song_dict = {
+        "song_name": song_names,
+        "artist_name": artist_names,
+        "played_at": played_at,
+        "timestamp": timestamps
+    }
+
+    # Convert dictionary to DataFrame
+    return pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
+
+
+def validate_data(df: pd.DataFrame) -> None:
+    """
+    Validate the DataFrame by checking for nulls, primary key violation,
+    and correct date.
+
+    Args:
+        df (pd.DataFrame): Data to be validated
+
+    Raises:
+        EmptyDataFrameError: If the input DataFrame is empty
+        PrimaryKeyViolationError: If the primary key check fails
+        NullValuesError: If null values are found in the input DataFrame
+        InvalidTimestampError: If at least one song has an invalid timestamp
+    """
+
+    # Check if the dataframe is empty
+    if df.empty:
+        raise EmptyDataFrameError("No songs downloaded.")
+
+    # Primary Key Check
+    if not pd.Series(df['played_at']).is_unique:
+        raise PrimaryKeyError("Primary key check is violated.")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise NullValuesError("Null values found in DataFrame.")
+
+    # Check that all timestamps are of today's date
+    today = dt.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    if any(dt.datetime.strptime(ts, "%Y-%m-%d") != today for ts in df["timestamp"]):
+        raise InvalidTimestampError("At least one song does not have today's timestamp")
+
+    print("Data valid, proceed to Load stage")
+
+
+
+def load_data(df: pd.DataFrame) -> None:
+    """
+    Loads data into SQLite database.
+
+    Args:
+        df (pandas.DataFrame): DataFrame containing song names, artist names,
+        played_at and timestamps.
+    """
+
+    # Load data into SQLite database
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect("my_played_tracks.sqlite")
+
+    # Create database table if it doesn't already exist
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor = conn.cursor()
+    cursor.execute(sql_query)
+    print("Opened database successfully")
+
+    # Load data into database table
+    try:
+        df.to_sql("my_played_tracks", engine, index=False, if_exists="append")
+    except sqlalchemy.exc.IntegrityError:
+        print("Data already exists in the database.")
+    finally:
+        # Close connection to database
+        conn.close()
+        print("Closed database successfully.")
+
+
+def main() -> None:
+    """
+    Main function that orchestrates the ETL process.
+    """
+
+    # Extract data from Spotify API
+    data = extract_recently_played()
+
+    # Transform data into pandas DataFrame
+    df = transform_data(data)
+    print(df)
+
+    # Validate data in the DataFrame
+    validate_data(df)
+
+    # Load data into SQLite database
+    load_data(df)
+
+    print("ETL process completed.")
+
+if __name__ == "__main__":
+    main()
